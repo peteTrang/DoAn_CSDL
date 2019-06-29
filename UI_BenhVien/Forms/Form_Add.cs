1b2d6ca68e9139a46a42a0e0185005bbe8a4796c
laochihu <laochihu@gmail.com>
2019-06-29 14:35:00 +0700

    try it bitch

--- UI_BenhVien/Forms/Form_Add.cs
+++ UI_BenhVien/Forms/Form_Add.cs
@@ -79,23 +79,54 @@ namespace UI_BenhVien.Forms
                 pnlChuyenNganh.Location = panel2.Location;
                 panel2.Location = pnlAddress.Location;
 
-                textBox1.Name = "txbChucDanh";
+                //textBox1.Name = "txbChucDanh";
 
             }
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-          /*
-           * Bệnh nhân:
-           *        textBox1 = ngày sinh
-           *        txbAddress =  địa chỉ
-           *        combobox2 = bệnh
-           *        
-           * Nhân viên:
-           *        combobox2 = tên khoa
-           *        txbChuyenNganh =  chuyên ngành
-           */
+            /*
+             * Bệnh nhân:
+             *        textBox1 = ngày sinh
+             *        txbAddress =  địa chỉ
+             *        combobox2 = bệnh
+             *        
+             * Nhân viên:
+             *        combobox2 = tên khoa
+             *        txbChuyenNganh =  chuyên ngành
+             *        textBox1 = Chức danh
+             */
+            using(QuanLyBenhVienDataContext db = new QuanLyBenhVienDataContext())
+            {
+                if(cbObject.SelectedIndex == 0)
+                {
+                    BenhNhan add = new BenhNhan();
+                    add.HotenBN = txbName.Text;
+                    add.NgaySinh = DateTime.ParseExact(textBox1.Text, "dd.MM.yyyy", null);
+                    add.GioiTinh = (cbSex.SelectedIndex == 0)? true : false;
+                    add.Diachi = txbAddress.Text;
+                    var a = (from benh in db.BenhLies
+                            where benh.TenBenh == comboBox2.SelectedText
+                            select benh.MaBenh).FirstOrDefault();
+                    add.MaBenh = a;
+                    db.BenhNhans.InsertOnSubmit(add);
+                    db.SubmitChanges();
+                }
+                if(cbObject.SelectedIndex == 1)
+                {
+                    NhanVien add = new NhanVien();
+                    add.HotenNV = txbName.Text;
+                    add.ChucDanh = textBox1.Text;
+                    add.TenKhoa = comboBox2.SelectedText;
+                    var a = (from cn in db.ChuyenNganhs
+                             where cn.TenChuyenNganh == cbChuyenNganh.SelectedText
+                             select cn.MaChuyenNganh).FirstOrDefault();
+                    add.MaChuyenNganh = a;
+                    db.NhanViens.InsertOnSubmit(add);
+                    db.SubmitChanges();
+                }
+            }
         }
     }
 }
